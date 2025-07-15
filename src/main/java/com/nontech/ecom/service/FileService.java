164bad128b49125264c139e932ac56d4ecedd337
ArunKr032Singh <arunkrasingh@gmail.com>
2025-07-16 01:14:32 +0530

    add login logout using jwt cookie

--- src/main/java/com/nontech/ecom/service/FileService.java
+++
@@ -1,9 +0,0 @@
-package com.nontech.ecom.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-
-public interface FileService {
-    String uploadImage(String path, MultipartFile image) throws IOException;
-}
